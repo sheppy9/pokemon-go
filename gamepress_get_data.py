962d342f8f543560d10bd6d29903279728fa1f60
kaiying <kaiying.lai@bitopiatech.com>
2024-09-04 10:05:07 +0800

    handle parse error and directory

--- gamepress_get_data.py
+++ gamepress_get_data.py
@@ -28,7 +28,7 @@ def get_pokemons():
 
 @ray.remote
 def get_pokemon_data(gamepass_pokemon_id):
-	json_filename = Path(f'{data_root}/{gamepass_pokemon_id}.json')
+	json_filename = Path(f'{data_root}/pokemon/{gamepass_pokemon_id}.json')
 	if json_filename.exists():
 		print(f'Loading pokemon data from file')
 		json_file = open(json_filename)
@@ -70,26 +70,77 @@ def get_pokemon_data(gamepass_pokemon_id):
 		if len(moves) == 2:
 			for move in moves[0]:
 				cd_info, dps_info, eps_info = move.find_all('div')[3].find_all('button')
+
+				cooldown = cd_info.text.replace('CD', '')
+				damage_per_second = dps_info.text.replace('DPS', '')
+				energy_per_second = eps_info.text.replace('EPS', '')
+				power = move.find_all('button')[4].text
+
+				try:
+					cooldown = float(cooldown)
+				except ValueError:
+					pass
+				try:
+					damage_per_second = float(damage_per_second)
+				except ValueError:
+					pass
+				try:
+					energy_per_second = float(energy_per_second)
+				except ValueError:
+					pass
+				try:
+					power = int(power)
+				except ValueError:
+					pass
+
 				data['fast_moves'].append({
 					'name': move.select_one('div:nth-child(2)').text,
 					'url': move.select_one('a')['href'],
-					'cool_down': float(cd_info.text.replace('CD', '')),
-					'damage_per_second': float(dps_info.text.replace('DPS', '')),
-					'energy_per_second': float(eps_info.text.replace('EPS', '')),
-					'power': int(move.find_all('button')[4].text)
+					'cool_down': cooldown,
+					'damage_per_second': damage_per_second,
+					'energy_per_second': energy_per_second,
+					'power': power
 				})
 
 			for move in moves[1]:
 				test = move.find_all('button')[1:5]
 				cd_info, dps_info, dpe_info, dw_info = move.find_all('button')[1:5]
+
+				cooldown = cd_info.text.replace('CD', '')
+				damage_per_second = dps_info.text.replace('DPS', '')
+				damage_per_energy = dpe_info.text.replace('DPE', '')
+				dodge_window = dw_info.text.replace('DW', '')
+				power = move.find_all('div')[-1].text
+
+				try:
+					cooldown = float(cooldown)
+				except ValueError:
+					pass
+				try:
+					damage_per_second = float(damage_per_second)
+				except ValueError:
+					pass
+				try:
+					damage_per_energy = float(damage_per_energy)
+				except ValueError:
+					pass
+				try:
+					dodge_window = float(dodge_window)
+				except ValueError:
+					pass
+				try:
+					power = int(power)
+				except ValueError:
+					pass
+
 				data['charge_moves'].append({
 					'name': move.select_one('div:nth-child(2)').text,
 					'url': move.select_one('a')['href'],
-					'cool_down': float(cd_info.text.replace('CD', '')),
-					'damage_per_second': float(dps_info.text.replace('DPS', '')),
-					'damage_per_energy': float(dpe_info.text.replace('DPE', '')),
-					'dodge_window': float(dw_info.text.replace('DW', '')),
-					'power': int(move.find_all('div')[-1].text)
+					'cool_down': cooldown,
+					'damage_per_second': damage_per_second,
+					'damage_per_energy': damage_per_energy,
+					'dodge_window': dodge_window,
+					'power': power
 				})
 
 		tables = soup.find('div', { 'id': 'type-chart' }).find_all('table')
