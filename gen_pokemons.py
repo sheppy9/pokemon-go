7ec4db02986c039655d2b1ea77fccfbf0fedab15
kaiying <kaiying.lai@bitopiatech.com>
2024-09-04 13:47:52 +0800

    Added more information in pokemon

--- gen_pokemons.py
+++ gen_pokemons.py
@@ -10,10 +10,17 @@ csv_data = []
 for file in glob(f'{pokemon_root}/*.json'):
 	data = json.load(open(file))
 	csv_data.append({
-		'Name': data['id'],
+		'ID': data['id'],
+		'Name': data['name'],
+		'Types': ', '.join(sorted(data['types'])),
+		'Evolution': ', '.join([_['name'] for _ in data['evolutions']]),
 		'Weaknesses': ', '.join(sorted([_['type'] for _ in data['weaknesses']])),
 		'Resistances': ', '.join(sorted([_['type'] for _ in data['resistances']])),
+		'Attack': data['attack'],
+		'Defense': data['defense'],
+		'Stamina': data['stamina'],
 	})
 
 df = pd.DataFrame(csv_data)
 df.to_json(f'{data_root}/json/pokemon.json', orient='records')
+print('Generate pokemon completed')
